e89386be4ec7a0cf85c9f5a576472dea5a7bd4a9
Doan Van Tung <tungdelx2@gmail.com>
2024-09-24 19:11:40 +0700

    Delete fragment and add changing back ground color event
    Add div directly to the container after the
    back ground color of div changed.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 "use strict";
 const container = document.querySelector(".container");
 
-// Add 16 divs to the DOM
-const fragment = document.createDocumentFragment();
-
 for (let i = 0; i < 256; i++) {
   const div = document.createElement("div");
-  div.classList.add("grid-card");
-  div.textContent = `Card ${i + 1}`;
-  fragment.appendChild(div);
-}
 
-container.appendChild(fragment);
+  div.classList.add("grid-card");
+
+  div.addEventListener("mouseover", () => {
+    div.style.backgroundColor = "#000";
+  });
+
+  container.appendChild(div);
+}
