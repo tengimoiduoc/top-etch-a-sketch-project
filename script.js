7d36afc0b5404cec78fb51ab955288438db50172
Doan Van Tung <tungdelx2@gmail.com>
2024-09-24 19:57:32 +0700

    Add and style two buttons

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict";
+const body = document.querySelector("body");
 const container = document.querySelector(".container");
 
 for (let i = 0; i < 256; i++) {
@@ -12,3 +13,25 @@ for (let i = 0; i < 256; i++) {
 
   container.appendChild(div);
 }
+
+// Add button that gets user's number of square
+const btnContainer = document.createElement("div");
+btnContainer.classList.add("btn-container");
+
+body.insertBefore(btnContainer, container);
+const containerBtn = document.querySelector(".btn-container");
+
+const btnGetSquare = document.createElement("btn");
+btnGetSquare.classList.add("btn", "btn-get-square");
+btnGetSquare.textContent = "Square";
+
+const btnClear = document.createElement("btn");
+btnClear.classList.add("btn", "btn-clear");
+btnClear.textContent = "Clear";
+
+containerBtn.appendChild(btnGetSquare);
+containerBtn.appendChild(btnClear);
+// 1. change the number of square === user input
+// when button is clicked
+//2. remove existing grid
+//3. new grid have the same total space
