4c53bbfe7c9d2678f816e1bfa0819b0ed44763dc
Doan Van Tung <tungdelx2@gmail.com>
2024-09-25 11:49:20 +0700

    Finish the project

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 "use strict";
-
 // Select elements from the DOM
 const bodyElement = document.querySelector("body");
 const gridContainer = document.querySelector(".container");
 const gridItems = document.querySelectorAll(".grid-card");
 
+// Function for creating new grid
 function createGrid(numberOfSquaresPerRow) {
+  // Remove Existing grid
   gridContainer.innerHTML = "";
 
   for (
@@ -16,12 +17,12 @@ function createGrid(numberOfSquaresPerRow) {
     const newGridItem = document.createElement("div");
 
     newGridItem.classList.add("grid-card");
+    newGridItem.textContent = "+";
+    newGridItem.style.flex = `0 1 calc(100%/${numberOfSquaresPerRow}`;
 
     newGridItem.addEventListener("mouseover", () => {
       newGridItem.style.backgroundColor = "#000";
     });
-
-    newGridItem.style.flex = `0 1 calc(100%/${numberOfSquaresPerRow}`;
 
     gridContainer.appendChild(newGridItem);
   }
@@ -43,23 +44,30 @@ getSquaresButton.textContent = "Set Grid Size";
 
 const clearGridButton = document.createElement("button");
 clearGridButton.classList.add("button", "clear-grid-button");
-clearGridButton.textContent = "Clear Grid";
+clearGridButton.textContent = "Clear";
 
 buttonArea.appendChild(getSquaresButton);
 buttonArea.appendChild(clearGridButton);
 
 const getSquaresButtonElement = document.querySelector(".get-squares-button");
+const clearButtonElement = document.querySelector(".clear-grid-button");
 
+// Get desired grid size from user
 getSquaresButtonElement.addEventListener("click", () => {
   let desiredGridSize = Number(
     prompt("Enter the desired number of squares (Max: 100)")
   );
 
-  if (desiredGridSize >= 100) {
+  if (desiredGridSize > 100) {
     desiredGridSize = Number(
       prompt("Invalid number! Please enter a value less than 100.")
     );
   }
 
   createGrid(desiredGridSize); // Recreate the grid with the new size
+});
+
+//Clear black color
+clearButtonElement.addEventListener("click", () => {
+  gridContainer.innerHTML = "";
 });
